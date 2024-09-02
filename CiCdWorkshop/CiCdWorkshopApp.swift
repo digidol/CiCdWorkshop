0c740f31567cb19ee3097ac8c5462c64fbd2605e
Neil Taylor [nst] (Staff) <nst@aber.ac.uk>
2024-09-02 16:17:26 +0100

    Initial commit

---
+++ CiCdWorkshop/CiCdWorkshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CiCdWorkshopApp.swift
+//  CiCdWorkshop
+//
+//  Created by Neil Taylor on 02/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct CiCdWorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
